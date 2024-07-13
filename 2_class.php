92863f28265240fcfec32a504053a268cd743e79
imran <zunaeede@gmail.com>
2024-07-13 18:01:21 +0600

    main - random class practice.

---
+++ 2_class.php
@@ -0,0 +1,22 @@
+<?php
+// question: make a class with child and redirect with parent.
+
+class ClassName{
+  public $name;
+
+  public function __construct($name)
+  {
+    $this-> name = $name;
+  }
+}
+
+class classChild extends ClassName{
+  public function message(){
+    echo "Hello {$this->name}";
+  }
+}
+
+$show = new classChild("imran");
+
+
+?>
