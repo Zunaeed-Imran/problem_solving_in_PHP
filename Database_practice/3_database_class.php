9cd52e899f879b6b41bcf289c3ae6b4b7f87f14c
imran <zunaeede@gmail.com>
2024-07-13 19:28:32 +0600

    main - practicing database not done yet.

--- Database_practice/3_database_class.php
+++ Database_practice/3_database_class.php
@@ -2,7 +2,14 @@
 // question:  Make a class, that connect with database and will do something, like (insert, delete, update, etc).
 
 
+class Database{
 
+  public function connect(){
+     new mysqli("localhost", "root", "", "class_practice");
+  }
+}
 
+$connect = new Database();
+$connect->connect();
 
 ?>
