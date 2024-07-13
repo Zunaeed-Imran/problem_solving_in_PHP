66ca6a2753e6cc9c571bdbab52ede46155be368d
imran <zunaeede@gmail.com>
2024-07-13 11:28:39 +0600

    main - solved the namta problem.

--- 1_namta.php
+++ 1_namta.php
@@ -1,8 +1,24 @@
 <?php
 // question: 10 gorar namta 50 porjonto.
 
-for ($i = 10; $i <= 50; $i += 10) {
-  for ($j = 1; $j <= $i; $j++) {
-    echo $j * 10 . "\n";
-  }
+for ($i = 10; $i <= 50; $i++) {
+  echo $i * 1 . " ";
+  echo "\n";
+  echo $i * 2 . " ";
+  echo "\n";
+  echo $i * 3 . " ";
+  echo "\n";
+  echo $i * 4 . " ";
+  echo "\n";
+  echo $i * 5 . " ";
+  echo "\n";
+  echo $i * 6 . " ";
+  echo "\n";
+  echo $i * 7 . " ";
+  echo "\n";
+  echo $i * 8 . " ";
+  echo "\n";
+  echo $i * 9 . " ";
+  echo "\n";
+  echo $i * 10 . " ";
 }
