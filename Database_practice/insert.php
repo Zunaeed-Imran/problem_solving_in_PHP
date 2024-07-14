01e03b73350a3dcc238728dd8bdf7af2a446b836
imran <zunaeede@gmail.com>
2024-07-14 11:51:18 +0600

    main - fix the OOP way use the variable.

--- Database_practice/insert.php
+++ Database_practice/insert.php
@@ -9,17 +9,9 @@ $mobile = $_REQUEST['mobile'];
 $sql = "INSERT INTO `classTest1` (`user`, `age`, `mobile`) VALUES ('$user', '$age', '$mobile')";
 
 if(isset($_REQUEST['submit'])){
-  mysqli_query($connect->connect(), $sql);
+  mysqli_query($this->connect(), $sql);
   header('location:index.php');
 }
-
-
-
-
-
-
-
-
 
 
 
