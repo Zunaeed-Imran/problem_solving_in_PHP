1a94a6a9140573da315c63073203eb5ac4d70ea0
imran <zunaeede@gmail.com>
2024-07-14 19:32:50 +0600

    main - not done yet the palindrome problem.

---
+++ 6_palindrome.php
@@ -0,0 +1,13 @@
+<?php
+// Question: Program to Check if a Given String is Palindrome.
+
+// defination: A palindrome number is a number that remains the same when its digits are reversed.
+
+$palinNum = 55;
+
+$expo = explode("",$palinNum);
+$rev = implode("",$expo);
+
+$joine = join($rev);
+
+?>
