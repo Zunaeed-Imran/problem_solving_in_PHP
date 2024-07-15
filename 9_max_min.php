9309ce0d87734f26a431f396b383132a296b18da
imran <zunaeede@gmail.com>
2024-07-15 17:10:05 +0600

    main - finally fix the maximum and the minimum problem.

--- 9_max_min.php
+++ 9_max_min.php
@@ -1,18 +1,11 @@
 <?php
-//  Question: Maximum and Minimum element of array
+//  Question: Maximum and Minimum element of array.
 
 $arr = array(10, 20, 60);
-$max = 0;
-$min = 0;
 
-for($i = 0; $i <= $arr; $i++){
-  
-  if($arr[$i] < $max){
-    echo "max number is {$max}";
-  }elseif($arr[$i] > $min){
-    echo "min number is {$min}";
-  }
-}
+$max = max($arr);
+$min = min($arr);
 
-
+echo "Max number is {$max}" . "\n";
+echo "Minimum number is {$min}";
 ?>
