fc6be19ec2c9795fd949d952f5abbe4030d71628
imran <zunaeede@gmail.com>
2024-07-15 18:05:32 +0600

    main - another example of array sum - and array frequency not done yet.

---
+++ 10_array_frequency.php
@@ -0,0 +1,15 @@
+<?php
+//  Question: Counting frequencies of array elements.
+
+//  means determining how many times each unique element appears in an array.
+//  $array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+//  1 appears 1 time
+//  2 appears 2 times
+
+$arr = array(1, 2, 1, 3, 4);
+
+for($i = 0; $i < $arr; $i++){
+  echo $i . "\n";
+}
+
+?>
---
+++ 11_sum_length_array.php
@@ -0,0 +1,14 @@
+<?php
+// question: sum of digit length in array.
+
+
+$arrNum = array(1, 20, 30, 50);
+$sum = 0;
+
+for($i = 0; $i < count($arrNum); $i++){
+  $sum+= $i;
+}
+
+echo $i;
+
+?>
