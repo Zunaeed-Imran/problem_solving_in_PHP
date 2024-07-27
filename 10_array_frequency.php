a837f20f733db90f733a2673859f2efeb2c9ed2c
imran <zunaeede@gmail.com>
2024-07-27 18:00:22 +0600

    main - add a inline command.

--- 10_array_frequency.php
+++ 10_array_frequency.php
@@ -8,6 +8,8 @@
 
 // we need `array_intersect` to compare array.
 
+// here we use array intersection, but better we should use nested loop.
+
 $arr1 = array(1, 2, 3, 4);
 $arr2 = array(3, 4, 5, 6);
 
@@ -16,3 +18,6 @@ $result = array_intersect($arr1, $arr2);
 foreach ($result as $item) {
   echo "\n" . $item . " are two times in the array. ";
 }
+
+
+?>
