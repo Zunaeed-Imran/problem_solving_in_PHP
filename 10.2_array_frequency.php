e055d03fad2d51076e4094b1cf09f82de4d6fd06
imran <zunaeede@gmail.com>
2024-07-27 18:14:26 +0600

    main - practicing new task.

---
+++ 10.2_array_frequency.php
@@ -0,0 +1,17 @@
+<?php
+//  Question: Counting frequencies of array elements.
+
+//  means determining how many times each unique element appears in an array.
+//  $array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+//  1 appears 1 time
+//  2 appears 2 times
+
+// we are gonna use nested loop.
+
+$arr1 = array(1, 2, 3, 4);
+$arr2 = array(3, 4, 5, 6);
+
+
+
+
+?>
