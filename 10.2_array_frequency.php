6e88671046b80a77f7e3daaecdaff207be855dc8
imran <zunaeede@gmail.com>
2024-07-29 16:23:49 +0600

    main - finally find the way of showing the match value.

--- 10.2_array_frequency.php
+++ 10.2_array_frequency.php
@@ -10,15 +10,22 @@
 
 $arr1 = array(1, 2, 3, 4);
 $arr2 = array(3, 4, 5, 6);
-
+$arr3 = array("all Matches");
 
 for ($i = 0; $i < count($arr1); $i++) {
 
   for ($j = 0; $j < count($arr2); $j++) {
+
     if ($arr1[$i] == $arr2[$j]) {
-      echo " match ";
+      array_push($arr3, $arr1[$i], $arr2[$j]);
+      //   echo " i:{$arr1[$i]} " . " " . " j:{$arr2[$j]} ";
     }
   }
 }
+
+foreach ($arr3 as $item) {
+  echo $item . " ";
+}
+
 
 ?>
