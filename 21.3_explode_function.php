bd29305176a07a9441f9d196a62c1707d5133cc1
imran <zunaeede@gmail.com>
2024-07-30 18:01:14 +0600

    main - add another example of explode function.

---
+++ 21.3_explode_function.php
@@ -0,0 +1,22 @@
+<?php
+// topic: explode funcion using list variable.
+//  we are gonna use list.
+// we don't use foreach loop this time.
+
+$data = "foo:*:1000:1080::/home/foo:/bin/sh";
+
+
+list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
+
+echo "user :{$user}" . "\n";
+echo "password: {$pass}" . "\n";
+echo "uid: {$uid}" . "\n";
+echo "gid: {$gid}" . "\n";
+echo "gecos: {$gecos}" . "\n";
+echo "home: {$home}" . "\n";
+echo "shell: {$shell}" . "\n";
+
+
+//  we use :: for $gecos because we don't want vslue in that variable
+
+?>
