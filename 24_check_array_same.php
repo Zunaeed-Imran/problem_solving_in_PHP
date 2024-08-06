8e2cdd1f5c415cd3aecfb6b4a59b0d9fe64a9784
imran <zunaeede@gmail.com>
2024-08-06 10:54:32 +0600

    main - solved two array are smae or not.

---
+++ 24_check_array_same.php
@@ -0,0 +1,15 @@
+<?php
+// question: Check if two arrays are the same or not.
+// array problem solving
+
+$arr1 = array(1, 2, 3, 4);
+$arr2 = array(1, 2, 3, 4);
+
+if($arr1 == $arr2){
+  echo "the array are the same";
+}else{
+  echo "the array are not the same";
+}
+
+
+?>
