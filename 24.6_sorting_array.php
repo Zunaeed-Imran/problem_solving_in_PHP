8d9553317169fc228afd3360f5edded638d3f2dd
imran <zunaeede@gmail.com>
2024-08-07 15:18:23 +0600

    main - done the array sort task.

---
+++ 24.6_sorting_array.php
@@ -0,0 +1,13 @@
+<?php
+// question: Sorting the elements of an array.
+// array programming.
+
+$arr = array(1, 6, 9, 5, 2, 3);
+
+sort($arr);
+
+foreach($arr as $item){
+  echo $item . "\n";
+}
+
+?>
