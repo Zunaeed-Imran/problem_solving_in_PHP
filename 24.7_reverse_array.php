966798603061b632698716000de0692f8fe869a2
imran <zunaeede@gmail.com>
2024-08-07 15:50:41 +0600

    main - done the task reverse array.

--- 24.7_reverse_array.php
+++ 24.7_reverse_array.php
@@ -1,7 +1,13 @@
 <?php
+// question: Reversing an array.
+// array programming.
 
+$arr = array(6, 5, 4, 3, 2, 1, 8, 9);
 
+rsort($arr);
 
-
+foreach ($arr as $item) {
+  echo $item . "\n";
+}
 
 ?>
