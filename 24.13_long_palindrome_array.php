a4bdc65b106bbd2acb61c172b9d2a95dd5a43681
imran <zunaeede@gmail.com>
2024-08-10 11:25:58 +0600

    main - finally done the task. - longest palindrome in an array.

--- 24.13_long_palindrome_array.php
+++ 24.13_long_palindrome_array.php
@@ -1,9 +1,9 @@
 <?php
-// question: Longest palindrome in an array./ not done the task.
+// question: Longest palindrome in an array.
 // array programming.
 
 $arr = array(121, 232, 525, 636, 526);
-
+$big = 0;
 
 foreach($arr as $item){
   $item; 
@@ -11,20 +11,16 @@ foreach($arr as $item){
   $rev = array_reverse($spl);
   $joint = implode($rev);
 
-  if($joint == $item){
-    // echo $item . "\n";
-    $array = str_split($item);
-    
-    rsort($array);
-    // // another loop
-    // foreach ($array as $i) {
-    //   echo $i . "\n";
-    // }
 
-    echo $array[0];
+  if($joint == $item){
+
+    if($item > $big){
+      $big = $item;
+    }
 
   }
 }
 
+echo "the max is {$big}";
 
 ?>
--- 24.2_array_type_find.php
+++ 24.2_array_type_find.php
@@ -1,5 +1,5 @@
 <?php
-// question: Finding the array type.
+// question: Finding the array type. / not done yet.
 // array problem solving.
 
 $arr = array(1, 2, 3, 4);
