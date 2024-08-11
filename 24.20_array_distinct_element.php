ec4e803ab0133d760b90fc8d2bc65aebc80e076e
imran <zunaeede@gmail.com>
2024-08-11 16:15:38 +0600

    main - done one task - show the distinct elements in an array.

--- 24.20_array_distinct_element.php
+++ 24.20_array_distinct_element.php
@@ -1,11 +1,11 @@
 <?php
-// question: Count distinct elements of an array.
+// question: Show the distinct elements in an array.
 // array programming.
+
 
 // explain question: Suppose you have an array: [1, 2, 2, 3, 4, 4, 4, 5]
 // The elements in this array are: 1, 2, 2, 3, 4, 4, 4, 5
 // The distinct (unique) elements are: 1, 2, 3, 4, 5
-// The count of these distinct elements is 5.
 
 $arr = array(1, 2, 2, 3, 4, 4, 4, 5);
 
@@ -15,5 +15,7 @@ $arr2 = array_unique($arr);
 foreach ($arr2 as $item) {
   echo $item . " ";
 }
+
+
 
 ?>
