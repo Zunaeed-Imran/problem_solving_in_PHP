cf5030d8032a9842859fbe5c78fc91dacbc3186a
imran <zunaeede@gmail.com>
2024-08-12 18:57:14 +0600

    main - not done yet.

---
+++ 24.24_symmantric_paris_array.php
@@ -0,0 +1,18 @@
+<?php
+// question: Symmetric pairs in an array. / not done yet.
+// array programming.
+
+// Example: Suppose the array of pairs is [(1, 2), (2, 1), (3, 4), (4, 5), (5, 4)].
+
+// The pairs (1, 2) and (2, 1) are symmetric because 1 == 1 and 2 == 2.
+// The pairs (3, 4) and (4, 3) would also be symmetric if they existed.
+// The pairs (4, 5) and (5, 4) are symmetric because 4 == 4 and 5 == 5.
+
+// So, the output would be:
+// [(1, 2), (2, 1)]
+// [(4, 5), (5, 4)]
+
+
+
+
+?>
---
+++ 24.25_array_disjoint.php
@@ -0,0 +1,15 @@
+<?php
+// question: Arrays are disjoint or not
+// array programming.
+
+// Example: Suppose you have the following two arrays:
+// array1 = [1, 2, 3, 4, 5]
+// array2 = [6, 7, 8, 9, 10]
+// These arrays are disjoint because they have no elements in common. The output would be true.
+
+$arr1 = array(1, 2, 3, 4, 5);
+$arr2 = array(6, 7, 8, 9, 10);
+
+
+
+?>
