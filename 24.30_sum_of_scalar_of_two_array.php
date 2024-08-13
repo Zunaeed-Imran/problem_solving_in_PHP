db9889b906669ff1c1c14440abfa5caf4fc73cb5
imran <zunaeede@gmail.com>
2024-08-13 18:58:26 +0600

    main - done the task. - sum of the scalar product.

--- 24.30_sum_of_scalar_of_two_array.php
+++ 24.30_sum_of_scalar_of_two_array.php
@@ -1,11 +1,11 @@
 <?php
-// question: scalar product of tow vector.
+// question: sum of scalar product of two vector.
 // array programming.
 
 // Example: Scalar Product
 // Vector A: [a1, a2, a3]
 // Vector B: [b1, b2, b3]
-
+// Scalar Product = (a1×b1) + (a2×b2) + (a3×b3)
 
 
 $a = array(1, 3, 5);
@@ -14,11 +14,14 @@ $b = array(2, 4, 1);
 //  we use array_combine to make scalar array.
 $c = array_combine($a, $b);
 $r = 0;
+$sum = 0;
 
 foreach ($c as $i => $j) {
   $r =  $i * $j;
-  echo $r . " ";
+  $sum += $r;
 }
+
+echo $sum;
 
 
 ?>
