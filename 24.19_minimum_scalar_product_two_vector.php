f4ad96b9d2b2213251f6d1cce3870359980fd18f
imran <zunaeede@gmail.com>
2024-08-13 19:39:20 +0600

    main - done the task. - minimum scalar product of tow vector.

--- 24.19_minimum_scalar_product_two_vector.php
+++ 24.19_minimum_scalar_product_two_vector.php
@@ -1,5 +1,5 @@
 <?php
-// question: Minimum scalar product of two vectors / not done yet.
+// question: Minimum scalar product of two vectors.
 // array programming.
 
 // Example: Scalar Product
@@ -17,6 +17,25 @@
 // Vector B: [4, 2, 1] (descending)
 // (1×4)+(3×2)+(5×1)=4+6+5=15
 
+$arr1 = array(1, 3, 5);
+$arr2 = array(2, 4, 1);
+$multiple = 0;
+$sum = 0;
+
+// sorting array, ascending & descending  to make the final value minimum.
+sort($arr1);
+rsort($arr2);
+
+// we use array_combine to make scalar array.
+$combine = array_combine($arr1, $arr2);
+
+foreach($combine as $item1 => $item2){
+  $multiple = $item1 * $item2;
+  // echo $multiple . " ";
+  $sum+= $multiple;
+}
+
+echo $sum;
 
 
 ?>
