39fe9742f9952138b390769c7902f6d1eda8fd8e
imran <zunaeede@gmail.com>
2024-08-14 12:13:48 +0600

    main - done one mroe tasks. - determine frequency of each elements.

---
+++ 24.33_sum_of_even_frequency.php
@@ -0,0 +1,35 @@
+<?php
+// question: Sum of all even frequency elements in an array. / not done yet
+// array programming.
+
+// Example:
+// array=[10,10,10,20,30,30,30,20]
+
+// Step 1: Determine the Frequency of Each Element:
+// Frequency of 10: 3 times
+// Frequency of 20: 2 times
+// Frequency of 30: 3 times
+// Step 2: Identify Elements with Even Frequencies:
+// 20 appears 2 times (even frequency)
+// Step 3: Sum the Elements with Even Frequencies:
+
+// Sum=20+20=40
+// Final Answer:
+// The sum of all elements with even frequencies in the array [10, 10, 10, 20, 30, 30, 30, 20] is 40.
+
+$arr = array(10, 10, 10, 20, 30, 30, 30, 20);
+$sum = 0;
+
+// we take array count to see how many times one elements repeated in a array.
+$count = array_count_values($arr);
+
+foreach ($count as $item => $times) {
+  if($times%2== 0){
+    $sum+= $item;
+
+  }
+}
+
+echo $sum;
+
+?>
---
+++ 24.34_determine_frequency.php
@@ -0,0 +1,24 @@
+<?php
+// question: Determine the Frequency of Each Element:
+// array programming.
+
+// Example explanation.
+// array=[10,10,10,20,30,30,30,20]
+
+// Step 1: Determine the Frequency of Each Element:
+// Frequency of 10: 3 times
+// Frequency of 20: 2 times
+// Frequency of 30: 3 times
+
+$arr = array(10, 10, 10, 20, 30, 30, 30, 20);
+$sum = 0;
+
+$count = array_count_values($arr);
+
+foreach($count as $item => $times){
+  echo "{$item} = {$times} times \n";
+}
+
+
+
+?>
