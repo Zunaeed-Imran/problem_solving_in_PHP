a7cb12c3733d0c8eae7706a562a59cd3d7cd81f9
imran <zunaeede@gmail.com>
2024-08-14 19:45:37 +0600

    main - add another question for solving.

---
+++ 24.37_rearrange_positive_negative_array.php
@@ -0,0 +1,29 @@
+<?php
+// question: Rearrange positive and negative numbers in an array. / not done yet.
+// array programming.
+
+
+// Example 1: Grouping Negatives and Positives Separately
+// Input Array:
+
+// array=[1,−2,3,−4,5,−6]
+
+// Desired Output:
+
+// array=[−2,−4,−6,1,3,5]
+
+// Example 2: Alternating Positive and Negative Numbers
+// Input Array:
+
+// array=[1,−2,3,−4,5,−6]
+
+// Desired Output:
+
+// array=[1,−2,3,−4,5,−6]
+
+// In this case, the output maintains an alternating pattern between positive and negative numbers.
+
+$arr = array(1, 2, 3, 4, 5, 6);
+
+
+?>
