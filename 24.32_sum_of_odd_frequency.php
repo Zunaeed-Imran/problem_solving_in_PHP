f5f78d2f096c8542b806aa8701a843c43f374809
imran <zunaeede@gmail.com>
2024-08-14 11:33:52 +0600

    main - finally done the task of sum of odd frequency elements in an array.

---
+++ 24.32_sum_of_odd_frequency.php
@@ -0,0 +1,47 @@
+<?php
+// question: Sum of all odd frequency elements in an array.
+// array programming.
+
+
+// Example 3:
+// array=[10,10,10,20,30,30,30,20]
+
+// Step 1: Determine the Frequency of Each Element:
+// Frequency of 10: 3 times
+// Frequency of 20: 2 times
+// Frequency of 30: 3 times
+// Step 2: Identify Elements with Odd Frequencies:
+// 10 appears 3 times (odd frequency)
+// 30 appears 3 times (odd frequency)
+// Step 3: Sum the Elements with Odd Frequencies:
+
+// Sum=10+30=40
+// Final Answer:
+// The sum of all elements with odd frequencies in the array [10, 10, 10, 20, 30, 30, 30, 20] is 40.
+
+$arr = array(10, 10, 10, 20, 30, 30, 30, 20);
+$sum = 0;
+
+$count = array_count_values($arr);
+
+foreach($count as $item => $times){
+  // echo $item . " = " . $times . ", ";
+  if($times%2!= 0){
+    $sum+= $item;
+
+  }
+}
+
+echo $sum;
+
+// for($i = 0; $i < count($arr); $i++){
+//   // echo $arr[$i] . " ";
+//   if($arr[$i]%2 == 0){
+//     echo "{$arr[$i]} are even \n" ;
+//   }elseif($arr[$i]%2 != 0){
+//     echo "{$arr[$i]} are Odd \n";
+//   }
+// }
+
+
+?>
