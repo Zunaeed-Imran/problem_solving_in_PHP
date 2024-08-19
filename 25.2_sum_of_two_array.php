08996c2edbb7d297a1d7b0c68b9df21a8a717655
imran <zunaeede@gmail.com>
2024-08-19 11:59:03 +0600

    main - solved one problem after a long time.

---
+++ 25.2_sum_of_two_array.php
@@ -0,0 +1,16 @@
+<?php
+// question: sum of two array elements value.
+// array programming.
+
+$arr1 = array(1, 2, 3, 4);
+$arr2 = array(5, 6, 7, 8);
+
+
+$sum1 = $arr1[0] + $arr2[0];
+$sum2 = $arr1[1] + $arr2[1];
+$sum3 = $arr1[2] + $arr2[2];
+$sum4 = $arr1[3] + $arr2[3];
+
+echo "sum of two array elements {$sum1} {$sum2} {$sum3} {$sum4}";
+
+?>
