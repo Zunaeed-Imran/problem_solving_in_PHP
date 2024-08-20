4dd8ec8da9a46e89f652567e776c3d0a55ea8509
imran <zunaeede@gmail.com>
2024-08-20 13:24:24 +0600

    main - done one more task - minimum element show in a 2d array column. - also edit inline command for another file.

---
+++ 25.7_minimum_element_column.php
@@ -0,0 +1,31 @@
+<?php
+// question: The minimum element in a column.
+// 2D array programming.
+
+
+// Example: 
+// a = [
+//   [1, 3, 7]
+//   [4, 2, 5]
+//   [9, 6, 8]
+// ]
+
+// max column 
+//  [
+//   1
+//   2
+//   6
+//  ]
+
+$arr = array(
+  min(array(1, 3, 7)),
+  min(array(4, 2, 5)),
+  min(array(9, 6, 8))
+);
+
+foreach($arr as $min_column){
+  echo $min_column . "\n";
+}
+
+
+?>
