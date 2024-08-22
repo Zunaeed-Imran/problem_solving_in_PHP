14b82cf1a1a1cfb7b7a125b7a93bb052399304e9
imran <zunaeede@gmail.com>
2024-08-22 18:52:22 +0600

    main - done one more task - sum of each column of a matrix.

--- 25.10_sum_of_each_column.php
+++ 25.10_sum_of_each_column.php
@@ -14,8 +14,17 @@
 // Column: 2+5+8=15
 // Column: 3+6+9=18
 
+$arr = array(
+  array(1, 2, 3),
+  array(4, 5, 6),
+  array(7, 8, 9)
+);
 
+$column1 = $arr[0][0] + $arr[1][0] + $arr[2][0];
+$column2 = $arr[1][0] + $arr[1][1] + $arr[1][2];
+$column3 = $arr[2][0] + $arr[1][0] + $arr[2][0];
 
+echo " {$column1} \n {$column2} \n {$column3}";
 
 
 ?>
