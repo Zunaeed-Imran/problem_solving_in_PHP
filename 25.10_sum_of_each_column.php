6f9f7f5092d6a89cb04605dd097c778bb136ac8e
imran <zunaeede@gmail.com>
2024-08-22 18:28:10 +0600

    main - done one task - sum of each row in a matrix. - other one is question.

---
+++ 25.10_sum_of_each_column.php
@@ -0,0 +1,21 @@
+<?php
+// question: sum of each column of a matrix.
+// 2D array programming.
+
+// example:
+// a = [
+//   [1, 2, 3]
+//   [4, 5, 6]
+//   [7, 8, 9]
+// ]
+
+// Sum of each column: 
+// Column: 1+4+7=12
+// Column: 2+5+8=15
+// Column: 3+6+9=18
+
+
+
+
+
+?>
---
+++ 25.9_sum_of_each_row_matrix.php
@@ -0,0 +1,29 @@
+<?php
+// question: Sum of each row of a matrix.
+// 2D array programming.
+
+// example: 
+// a = [
+//   [1, 2, 3]
+//   [4, 5, 6]
+//   [7, 8, 9]
+// ]
+
+// Sum of each row:
+//Row1: 1+2+3=6
+//Row2: 4+5+6=15
+//Row3: 7+8+9=24
+
+$arr = array(
+  array_sum(array(1, 2, 3)),
+  array_sum(array(4, 5, 6)),
+   array_sum(array(7, 8, 9))
+);
+
+
+foreach($arr as $row => $item){
+  echo "Sum of Row: {$item} \n";
+}
+
+
+?>
