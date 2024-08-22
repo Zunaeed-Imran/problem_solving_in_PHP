41765768292d41e2ec5dfe2ce3e51887a4bc528a
imran <zunaeede@gmail.com>
2024-08-22 19:28:44 +0600

    main - add two question will solved letter.

---
+++ 25.11_sum_of_boundry_array.php
@@ -0,0 +1,24 @@
+<?php
+// question: Sum of boundary elements of a matrix / not done yet.
+// 2D array programming.
+
+// example: 
+// [1, 2, 3, 4]
+// [5, 6, 7, 8]
+// [9, 10, 11, 12]
+// [13, 14, 15, 16]
+
+// Top row:           1, 2, 3, 4
+// Bottom row:        13, 14, 15, 16
+// Left column:       5, 9
+// Right column:      8, 12
+// boundry element are: 1,2,3,4,8,12,16,15,14,13,9,5.
+// sum of boundry element: 1+2+3+4+8+12+16+15+14+13+9+5=102
+
+
+
+
+
+
+
+?>
---
+++ 25.12_matrix_aspiral_form.php
@@ -0,0 +1,30 @@
+<?php
+// question: Matrix printing in aspiral form. / not done yet.
+// 2D array programming.
+
+
+// example: 
+// [1, 2, 3, 4]
+// [5, 6, 7, 8]
+// [9, 10, 11, 12]
+// [13, 14, 15, 16]
+
+// Start at the top-left:             1, 2, 3, 4
+// Move down the last column:         8, 12, 16
+// Move left across the bottom row:   15, 14, 13
+// Move up the first column:          9, 5
+// Move right again (inside layer):   6, 7 
+// Move down:                         11
+// Move left:                         10
+
+// The spiral order for this matrix would be: 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
+
+
+
+
+
+
+
+
+
+?>
