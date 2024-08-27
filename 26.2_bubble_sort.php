2492f76e6c87c0dd1379a637c6a80bb642e684b8
imran <zunaeede@gmail.com>
2024-08-27 15:12:09 +0600

    main - not done yet bubble sort.

---
+++ 26.2_bubble_sort.php
@@ -0,0 +1,23 @@
+<?php
+// question: sort an array using bubble sort.
+// array programming.
+
+
+$arr = array(5, 3, 8, 4, 2);
+$a = 0;
+$b = 0;
+$c = 0;
+$d = 0;
+$e = 0;
+
+if ($arr[0] > $arr[1] || $arr[0] < $arr[1] 
+    && $arr[2] > $arr[3] || $arr[2] > $arr[3]) {
+  $a = $arr[0];
+  $b = $arr[1];
+  $c = $arr[2];
+  $d = $arr[3];
+  // $e = $arr[0];
+
+} 
+
+var_dump($a, $b, $c, $d);
