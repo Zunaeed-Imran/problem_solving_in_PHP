ee727e2b267bba33117fdd59f5299e7299e6fa9d
imran <zunaeede@gmail.com>
2024-08-31 20:12:51 +0600

    main - practice 10-50 multiplayer using function.

---
+++ Database_practice/1.2_namta.php
@@ -0,0 +1,18 @@
+<?php
+// question: will show 10-50 gorar namta(multiplayer).
+// practice program using function.
+
+function Namta($a){
+  for ($i = 1; $i <= 10; $i++) { 
+    echo $i * $a . "\n";
+  }
+}
+
+Namta(10);
+Namta(20);
+Namta(30);
+Namta(40);
+Namta(50);
+
+
+?>
