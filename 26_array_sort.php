4b3783d181ea85ea79a096630b933979e64f07ce
imran <zunaeede@gmail.com>
2024-09-01 16:13:09 +0600

    main - done try for today array sorting. - not done yet.

--- 26_array_sort.php
+++ 26_array_sort.php
@@ -3,20 +3,17 @@
 // array programming.
 
 $arr = array(45, 29, 69, 12, 10, 5, 14);
-$sort1 = 0;
-$sort2 = 0;
-$sort3 = 0;
+$big = 0;
 
-
-for ($i = 0; $i < count($arr); $i++) {
-  if ($arr[$i] > $arr[0]) {
-    $sort1 = $arr[$i];
-    echo $sort1 . "\n";
-  }elseif($arr[$i] > $arr[1]){
-    $sort2 = $arr[$i];
-    echo $sort2 . "\n";
-  }elseif($arr[$i] > $arr[2]){
-    $sort3 = $arr[$i];
-    echo $sort3 . "\n";
+foreach ($arr as $item) {
+  if($item > $big){
+    $big = $item;
+  }
+  for ($i = 0; $i  <$item; $i++) { 
+    echo $i . " ";
   }
 }
+
+
+
+?>
