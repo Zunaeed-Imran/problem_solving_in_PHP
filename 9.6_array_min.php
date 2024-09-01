b76b00091d5666547ccf1f40f0e4bb9f809f5a55
imran <zunaeede@gmail.com>
2024-09-01 12:06:26 +0600

    main - done anotehr way of solved the min alements in anarray - also use function.

--- 9.6_array_min.php
+++ 9.6_array_min.php
@@ -1,6 +1,12 @@
 <?php
-// 
-// array programming 
+// question: show minimum elements in an array. another way(using sort function). also using function.
+// array programming also use function.
 
+function MinArray($arr){
+  sort($arr);
+  echo $arr[0];
+}
+
+MinArray([10, 20, 636, 558, 965, 265, 555, 50]);
 
 ?>
