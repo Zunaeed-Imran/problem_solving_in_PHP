711e443b6fb33814156c7bc2ebb4e0835d895180
imran <zunaeede@gmail.com>
2024-09-09 18:57:49 +0600

    main - fix the problem by dolon da.

--- 25.5.2_matrix_maximum_elements_row.php
+++ 25.5.2_matrix_maximum_elements_row.php
@@ -17,17 +17,30 @@ $arr = array(
   array(4, 2, 5),
   array(9, 6, 8)
 );
-$sum = 0;
+$sum1 = 0;
+$sum2 = 0;
+$sum3 = 0;
 
 
-foreach ($arr as $item) {
-  foreach ($item as $list){
-   $sum+=$list;
+foreach ($arr[0] as $item) {
+   $sum1+=$item;
   }
+foreach ($arr[1] as $item) {
+   $sum2+=$item;
+  }
+foreach ($arr[2] as $item) {
+   $sum3+=$item;
+  }
+
+
+
+if (($sum1 > $sum2) && ($sum1 > $sum3)) {
+  echo $sum1;
+} else if (($sum2 > $sum1) && ($sum2 > $sum3)) {
+  echo $sum2;
+} else if (($sum3 > $sum2) && ($sum3 > $sum1)) {
+  echo $sum3;
 }
 
-foreach ($sum as $result) {
-  echo $result . " ";
-}
 
 ?>
