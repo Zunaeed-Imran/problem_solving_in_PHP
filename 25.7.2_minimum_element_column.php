abadf34c4ffbb4ac3a5995f3ba9fdfcfb9e742ec
imran <zunaeede@gmail.com>
2024-09-10 11:34:09 +0600

    main - solved another problem without using in-built function. - but have some confusion about  the logic.

---
+++ 25.7.2_minimum_element_column.php
@@ -0,0 +1,52 @@
+<?php
+// question: The minimum element in a column, without using in-built function. 
+// 2D array programming.
+
+// Example: 
+// a = [
+//   [1, 3, 7]
+//   [4, 2, 5]
+//   [9, 6, 8]
+// ]
+
+// min column 
+//  [
+//   1
+//   2
+//   6
+//  ]
+
+$arr =  array(
+  array(1, 3, 7),
+  array(4, 2, 5),
+  array(9, 6, 8)
+);
+
+
+
+$small1 = $arr[0];
+$small2 = $arr[1];
+$small3 = $arr[2];
+
+
+foreach ($arr[0] as $item) {
+  if($item < $small1){
+    $small1 = $item;
+  }
+}
+foreach($arr[1] as $item){
+  if($item < $small2){
+    $small2 = $item;
+  }
+}
+foreach($arr[2] as $item){
+  $small3 = $item;
+}
+
+
+echo $small1 . "\n";
+echo $small2 . "\n";
+echo $small3 . "\n";
+
+
+?>
